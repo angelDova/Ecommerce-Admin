40a64b20690bdede4243271762dbba057ec6bbe1
angelDova <Angelfcb01@yahoo.com>
2023-06-17 20:57:32 -0700

    update

---
+++ components/Layout.jsx
@@ -0,0 +1,30 @@
+import { useSession, signIn, signOut } from "next-auth/react";
+import Nav from "@/components/Nav";
+
+export default function Layout({ children }) {
+  const { data: session } = useSession();
+  if (!session) {
+    return (
+      <div className="bg-blue-900 w-screen h-screen flex items-center">
+        <div className="text-center w-full">
+          <button
+            // add provider ID ('google') so it doesn't redirect you to log in to nextauth google
+            onClick={() => signIn("google")}
+            className="bg-white p-2 rounded-lg"
+          >
+            Login with Google
+          </button>
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <div className="bg-blue-900 min-h-screen flex">
+      <Nav />
+      <div className="bg-white flex-grow my-2 mr-2 rounded-lg p-4">
+        {children}
+      </div>
+    </div>
+  );
+}
