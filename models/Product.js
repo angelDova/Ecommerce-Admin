fba74bb3b2c5117d2c6bf03b4d8b7e3f3f9805bd
angelDova <Angelfcb01@yahoo.com>
2023-06-22 13:54:57 -0700

    timestamp

--- models/Product.js
+++ models/Product.js
@@ -1,13 +1,18 @@
 import mongoose, { Schema, model, models } from "mongoose";
 
-const ProductSchema = new Schema({
-  title: { type: String, required: true },
-  description: String,
-  price: { type: Number, required: true },
-  images: [{ type: String }],
-  category: { type: mongoose.Types.ObjectId, ref: "Category" },
-  properties: { type: Object },
-});
+const ProductSchema = new Schema(
+  {
+    title: { type: String, required: true },
+    description: String,
+    price: { type: Number, required: true },
+    images: [{ type: String }],
+    category: { type: mongoose.Types.ObjectId, ref: "Category" },
+    properties: { type: Object },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 // capital P???
 export const Product = models.Product || model("Product", ProductSchema);
