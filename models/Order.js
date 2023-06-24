1eb048b8ba31e54fd19dad4c9ab7ee60ac4d9dcc
angelDova <Angelfcb01@yahoo.com>
2023-06-23 20:52:20 -0700

    update

---
+++ models/Order.js
@@ -0,0 +1,20 @@
+import { model, models, Schema } from "mongoose";
+
+const OrderSchema = new Schema(
+  {
+    line_items: Object,
+    name: String,
+    email: String,
+    city: String,
+    state: String,
+    zipCode: String,
+    streetAddress: String,
+    country: String,
+    paid: Boolean,
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const Order = models?.Order || model("Order", OrderSchema);
